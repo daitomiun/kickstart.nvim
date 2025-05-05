f3b6394aa130cd08a5a70ed772dab7b302166079
daitomiun <mateo.rodriguez.fierro@gmail.com>
2025-05-04 19:06:28 -0500

    Add LOVE/lua plugin

--- init.lua
+++ init.lua
@@ -306,6 +306,16 @@ require('lazy').setup({
       },
     },
   },
+  {
+    "S1M0N38/love2d.nvim",
+    event = "VeryLazy",
+    opts = { },
+    keys = {
+      { "<leader>v", ft = "lua", desc = "LÖVE" },
+      { "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+      { "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
+    },
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
