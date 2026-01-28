1904f07e2597483fe20bfcca2802aa06c381d547
daitomiun <mateo.rodriguez.fierro@gmail.com>
2026-01-27 20:10:20 -0500

    Svelte LSP added

--- init.lua
+++ init.lua
@@ -759,6 +759,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
+        svelte = {},
         ts_ls = {},
         cssls = {},
         css_variables = {},
